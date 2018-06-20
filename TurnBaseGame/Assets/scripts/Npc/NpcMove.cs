781c27f9ec6407c5e7430543c28de1a5bdbf3fd1
picoolu <michael.hadas.r@gmail.com>
2018-06-21 00:43:41 +0300

    pathfinding with a star

---
+++ TurnBaseGame/Assets/scripts/Npc/NpcMove.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMove : TacticsMove
+{
+    GameObject target;
+
+	
+	void Start ()
+    {
+        Init();
+	}
+	
+	void Update ()
+    {
+        if (!turn)
+        {
+            return;
+        }
+
+
+        if (!moving)
+        {
+            FindNearestTarget();
+            CalculatePath();
+            FindSelectableTiles();
+            actualTargetTile.target = true;
+           
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    void CalculatePath()
+    {
+        Tile targetTile = GetTargetTile(target);
+        FindPath(targetTile);
+
+    }
+    
+    void FindNearestTarget()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
+
+        //finds the closest target to the Npc
+
+        GameObject nearest = null;
+        // algorithm to find the closest obj
+        float distance = Mathf.Infinity;
+
+        foreach (GameObject obj in targets)
+        {
+            float d = Vector3.Distance(transform.position, obj.transform.position);
+
+            if (d < distance)
+            {
+                distance = d;
+                nearest = obj;
+            }
+
+        }
+
+        target = nearest;
+    }
+
+}
