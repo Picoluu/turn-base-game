97312d71a2386bb92efd12ab4c3b3c86e2b3d99a
Picoolu <michaelhadasr@gmail.com>
2018-06-24 18:11:36 +0300

    first fase of the action bar

--- TurnBaseGame/Assets/heros/Hero.cs
+++ TurnBaseGame/Assets/heros/Hero.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New item",menuName = "ActionBar/Hero")]
 public class Hero : ScriptableObject
  {
+
+    public ActionBarSlots Slot = ActionBarSlots.NotOnActionBar;
+
     new public string name = "New item";
     public Sprite icon = null;
-    
+
+    public virtual void Use()
+    {
+        // useing the hero (probly equping to actionBar)
+        // add a reference to action bar andd add the hero to it 
+
+        Debug.Log("equping" + name);
+    }
+
  }
---
+++ TurnBaseGame/Assets/scripts/ActionBar/ActionBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBar : MonoBehaviour {
+
+    #region Singleton
+
+    public static ActionBar instance;
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Debug.LogWarning("more then one instance of ActionBar");
+            return;
+        }
+
+        instance = this;
+    }
+
+
+
+
+
+    #endregion
+
+    Hero[] currentEquiped;
+
+
+    private void Start()
+    {
+        int numSlots = System.Enum.GetNames(typeof(ActionBarSlots)).Length;
+        currentEquiped = new Hero[numSlots];
+    }
+
+
+    public void Equip(Hero newHero)
+    {
+        int slotIndex = (int)newHero.Slot - 1;
+
+        currentEquiped[slotIndex -1] = newHero;
+    }
+
+}
+
+public enum ActionBarSlots {FirstHero,SecendHero,TheirdHero,ForthHero,NotOnActionBar}
+
+
+
